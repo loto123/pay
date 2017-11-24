3a2d27351cfb61f8d31cb464204420942e6eb77e
luobailiang <luobailiang@gmail.com>
2017-11-24 10:23:34 +0800

    vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,5 +18,9 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    template: '<div>{{ test }}</div>',
+    data: {
+        test: 'hello, world'
+    }
 });
