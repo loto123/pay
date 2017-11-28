0f63377046a6f969b9d993e23d42270a7dc52604
luobailiang <luobailiang@gmail.com>
2017-11-28 15:45:04 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('auth/login', 'Api\AuthController@login');
+Route::post('auth/register', 'Api\AuthController@register');
+
+Route::any('test', 'API\TestController@index');
