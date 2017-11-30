4c5ed7863678d324a4cdd1601744603b7f093df0
liangtao <1174945650@qq.com>
2017-11-30 10:07:08 +0800

    update

--- resources/assets/js/view/MyAccount/myAccount.vue
+++ resources/assets/js/view/MyAccount/myAccount.vue
@@ -1,17 +1,10 @@
 <!-- 我的账户 -->
 <template>
     <div class="myAccount-container">
-        <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
-            <router-link to="/" slot="left">
-                <mt-button icon="back">返回321</mt-button>
-            </router-link>
-            <mt-button icon="more" slot="right"></mt-button>
-        </mt-header>
-    
-        <section class="withDraw-money-box">
+        <div class="withDraw-money-box">
             <div class="withDraw-money">231321</div>
-        </section>
-
+            <div>当前可余额</div>
+        </div>
         <div>
             <a href="javascript:;" class="withdraw_btn">
                 <mt-button size="large" type="primary">提现</mt-button>    
@@ -41,9 +34,6 @@
         height: 15em;
         border: 1px solid #aaa;
         border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
         margin: 3em auto 3em auto;
         background: #26a2ff;
         color: #fff;
@@ -51,17 +41,13 @@
             text-align: center;
         }
     }
-    .withdraw_btn{
+    .withdraw_btn,.recharge_btn{
         display: block;
         width: 100%;
         width: 96%;
         margin: auto;
     }
     .recharge_btn{
-        display: block;
-        width: 100%;
-        width: 96%;
-        margin: auto;
         margin-top: 2em;
     }
 </style>
