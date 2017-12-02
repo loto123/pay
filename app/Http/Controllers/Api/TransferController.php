f81d1586bc8adfaddcdf5dbfa155c239d57b40b8
nielixin <303896522@qq.com>
2017-12-02 18:22:14 +0800

    验证过滤器多语言

--- app/Http/Controllers/Api/TransferController.php
+++ app/Http/Controllers/Api/TransferController.php
@@ -9,32 +9,27 @@ use Validator;
 
 class TransferController extends Controller
 {
-    protected $user;
-
     public function __construct()
     {
-//        $this->middleware("jwt.auth");
-//        $this->user = JWTAuth::parseToken()->authenticate();
-//        debug($this->user);
+        $this->middleware("jwt.auth");
     }
 
     public function create(Request $request)
     {
+        $this->user = JWTAuth::parseToken()->authenticate();
+
         $validator = Validator::make($request->all(),
             [
                 'shop_id' => 'bail|required',
-                'price' => 'bail|required|numeric|min:0.1|max:99999',
+                'price' => 'bail|required|numeric|between:0.1,99999',
             ],
             [
                 'required' => trans('trans.required'),
-                'numeric' => 'trans.numeric',
-                'between' => 'trans.between',
-//                'price.max' => trans('price.max'),
-//                'price.required' => trans('price.required'),
-//                'shop_id.required' => trans('shop_id.required'),
-//                'shop_id.required' => trans('shop_id.required'),
+                'numeric' => trans('trans.numeric'),
+                'between' => trans('trans.between'),
             ]
         );
+
         if ($validator->fails()) {
             return response()->json(['code' => 0,'msg' => $validator->errors()->first(),'data' => []]);
         }
