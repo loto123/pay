fd93fb5c0fc8ecad4131021ddb374d0c05217326
lijuan <lijuan@supernano.com>
2017-12-06 10:03:54 +0800

    后台，用户管理，店铺管理模块

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,5 +11,9 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
-
+    $router->resource('users',UserController::class);
+    $router->any('/user/detail/{id}','UserController@details');
+    $router->any('shop','ShopController@index');
+    $router->any('/shop/detail/{shop_id?}','ShopController@details');
+    $router->any('/shop/updates','ShopController@updates');
 });
---
+++ resources/views/admin/userDetail.blade.php
@@ -0,0 +1,26 @@
+    <div class="container-fluid">
+        <div class="box box-widget widget-user-2">
+            <div class="box-header">
+                <div class="widget-user-header">
+                    <h4 class="widget-user-desc">编号：<span>{{$list->id}}</span></h4>
+                    <h4 class="widget-user-desc">用户名：<span>{{'22545555'}}</span></h4>
+                    <h4 class="widget-user-desc">昵称：<span>{{'哇哈哈'}}</span></h4>
+                    <h4 class="widget-user-desc">头像：<img src="{{'zzz'}}" width="40" height="40" class="img-circle"></h4>
+                    <h4 class="widget-user-desc">手机号码：<span>{{'123456789453'}}</span></h4>
+                    <h4 class="widget-user-desc">绑定微信：<span>{{'kjff'}}</span></h4>
+                    <h4 class="widget-user-desc">余额：<span>{{'55'}}</span></h4>
+                    <h4 class="widget-user-desc">收益：<span>{{'70'}}</span></h4>
+                    <h4 class="widget-user-desc">付款：<span>{{'44'}}</span></h4>
+                    <h4 class="widget-user-desc">收款：<span>{{'55'}}</span></h4>
+                    <h4 class="widget-user-desc">交易笔数：<span>{{'19'}}</span></h4>
+                    <h4 class="widget-user-desc">上级代理ID：<span>{{'123456'}}</span></h4>
+                    <h4 class="widget-user-desc">上级运营ID：<span>{{'123456'}}</span></h4>
+                    <h4 class="widget-user-desc">已付交易费：<span>{{'444'}}</span></h4>
+                    <h4 class="widget-user-desc">银行卡：<span>{{'中国银行'}}尾数{{'4455'}}{{'王小二'}}</span></h4>
+                    <h4 class="widget-user-desc">创建时间：<span>{{$list->created_at}}</span></h4>
+                    <button type="button" class="btn btn-primary" onclick="javascript:history.go(-1);">返回</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
