5c97353a1409fc02ff943884262773d04dcd0c25
lesliewung <vocn2010@gmail.com>
2017-12-20 11:13:14 +0800

    加载自定义Grid组件SwitchDisplay.php

---
+++ app/Admin/Displayers/SwitchDisplay.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Admin\Displayers;
+
+use Encore\Admin\Admin;
+
+class SwitchDisplay extends \Encore\Admin\Grid\Displayers\SwitchDisplay
+{
+    public function display($states = [])
+    {
+        $this->updateStates($states);
+
+        $name = $this->column->getName();
+
+        $class = "grid-switch-{$name}";
+
+        $script = <<<EOT
+
+$('.$class').bootstrapSwitch({
+    size:'mini',
+    onText: '{$this->states['on']['text']}',
+    offText: '{$this->states['off']['text']}',
+    onColor: '{$this->states['on']['color']}',
+    offColor: '{$this->states['off']['color']}',
+    onSwitchChange: function(event, state){
+
+        $(this).val(state ? 'on' : 'off');
+
+        var pk = $(this).data('key');
+        var value = $(this).val();
+        var \$cur_switch = $(this);
+        $.ajax({
+            url: "{$this->grid->resource()}/" + pk,
+            type: "POST",
+            data: {
+                $name: value,
+                _token: LA.token,
+                _method: 'PUT'
+            },
+            success: function (data) {
+                toastr[data.status === true ? 'success':'error'](data.message + "2");
+                data.status !== true && \$cur_switch.bootstrapSwitch('state', false, true);
+            }
+        });
+    }
+});
+
+EOT;
+
+        Admin::script($script);
+
+        $key = $this->row->{$this->grid->getKeyName()};
+
+        $checked = $this->states['on']['value'] == $this->value ? 'checked' : '';
+
+        return <<<EOT
+        <input type="checkbox" class="$class" $checked data-key="$key" />
+EOT;
+    }
+}
