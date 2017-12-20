51767fc9cc5c502ff81b7a7ff43055aba616d2d4
nielixin <303896522@qq.com>
2017-12-20 15:52:08 +0800

    修改交易接口

--- app/TransferUserRelation.php
+++ app/TransferUserRelation.php
@@ -9,10 +9,10 @@ class TransferUserRelation extends Model
     protected $table = 'transfer_user_relation';
 
     public function transfer() {
-        return $this->belongsTo('App\Transfer', 'id', 'transfer_id');
+        return $this->belongsTo('App\Transfer', 'transfer_id', 'id');
     }
 
     public function user() {
-        return $this->belongsTo('App\User', 'id', 'user_id');
+        return $this->belongsTo('App\User', 'user_id', 'id');
     }
 }
