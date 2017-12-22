9054322f62c39438806ded92b1628f8967016542
lesliewung <vocn2010@gmail.com>
2017-12-22 16:57:39 +0800

    支付场景配置 支付方式操作系统配置 充值方式列表 提现方式列表

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Pay\Model\Channel;
+use App\Pay\Model\DepositMethod;
+use App\Pay\Model\Scene;
 use App\User;
 use Illuminate\Http\Request;
-use JWTAuth;
 use Illuminate\Support\Facades\Validator;
+use JWTAuth;
 
 /**
  *
@@ -25,7 +28,7 @@ class AccountController extends BaseController {
     public function index(){
         $user = $this->auth->user();
         /* @var $user User */
-        return $this->json(['balance' => (float)$user->container->balance]);
+        return $this->json(['balance' => (float)$user->balance]);
     }
 
     /**
@@ -59,5 +62,56 @@ class AccountController extends BaseController {
      */
     public function withdraw() {
         return $this->json();
+    }
+
+
+    /**
+     * @param $os
+     * @param $scene
+     * @return mixed
+     */
+    public function payMethods($os, $scene)
+    {
+        /**
+         * @var $channelBind Channel
+         */
+        $os = $os == 'unknown' ? $os : ['ios' => DepositMethod::OS_IOS, 'andriod' => DepositMethod::OS_ANDRIOD][$os];
+
+        $scene = Scene::find($scene);
+        if ($os && $scene) {
+            $channelBind = $this->user->channel;
+            $channelBind = $channelBind ? $channelBind : Channel::find(1);
+            if ($channelBind->disabled) {
+                //被禁用则启用备用通道
+                $channelBind = $channelBind->spareChannel;
+            }
+
+            $methods = $channelBind->platform->depositMethods()->where('disabled', 0)->select('id', 'os', 'scene', 'show_label')->get();
+
+            return $this->json(['channel' => $channelBind->getKey(), 'methods' => $methods->filter(function ($method) use ($scene, $os) {
+                return in_array($scene->getKey(), $method->scene) &&  //支付场景筛选
+                    ($os == 'unknown' || $method->os == DepositMethod::OS_ANY || $method->os == $os);//未知系统,或不限系统,或系统匹配
+            })->mapWithKeys(function ($item) {
+                return [$item['id'] => $item['show_label']];
+            })]);
+        } else {
+            return $this->json(null, '不存在的场景或系统', 0);
+        }
+    }
+
+    public function withdrawMethods()
+    {
+        /**
+         * @var $channelBind Channel
+         */
+        $channelBind = $this->user->channel;
+        $channelBind = $channelBind ? $channelBind : Channel::find(1);
+        if ($channelBind->disabled) {
+            //被禁用则启用备用通道
+            $channelBind = $channelBind->spareChannel;
+        }
+
+        return $this->json($channelBind->platform->withdrawMethods()->where('disabled', 0)->select('id', 'show_label as label')->get());
+
     }
 }
