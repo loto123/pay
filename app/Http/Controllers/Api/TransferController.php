a5cdbb71800687c75495404760581355cd7afc88
nielixin <303896522@qq.com>
2017-12-25 18:21:47 +0800

    修改交易好友通知接口

--- app/Http/Controllers/Api/TransferController.php
+++ app/Http/Controllers/Api/TransferController.php
@@ -594,7 +594,7 @@ class TransferController extends Controller
         if ($transfer->status == 3) {
             return response()->json(['code' => 0, 'msg' => trans('trans.trans_already_closed'), 'data' => []]);
         }
-        if (TransferUserRelation::where('transfer_id', $transfer->transfer_id)->where('user_id', $request->friend_id)->exists()) {
+        if (TransferUserRelation::where('transfer_id', $transfer->id)->where('user_id', $request->friend_id)->exists()) {
             return response()->json(['code' => 0, 'msg' => trans('trans.notice_already_exists'), 'data' => []]);
         }
         DB::beginTransaction();
@@ -603,7 +603,7 @@ class TransferController extends Controller
                 $real_id = Skip32::decrypt("0123456789abcdef0123", $value);
                 if (!$transfer->joiner()->where('user_id', $real_id)->exists()) {
                     $relation = new TransferUserRelation();
-                    $relation->transfer_id = $transfer->transfer_id;
+                    $relation->transfer_id = $transfer->id;
                     $relation->user_id = $real_id;
                     $relation->save();
                 }
