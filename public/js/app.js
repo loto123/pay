15709faef55c0e448bda60f086ff9aeff70e5caf
sangliang <378305868@qq.com>
2017-12-25 10:56:23 +0800

    更新  调试微信登录

--- public/js/app.js
+++ public/js/app.js
@@ -74197,15 +74197,23 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
     methods: {
         init: function init() {
+            var _this = this;
+
             this.code = __WEBPACK_IMPORTED_MODULE_1__utils_utils__["a" /* default */].getQueryString("code");
             this.state = __WEBPACK_IMPORTED_MODULE_1__utils_utils__["a" /* default */].getQueryString("state");
-            console.log(this.code);
-            console.log(this.state);
+
             var _data = {
                 code: this.code,
                 state: this.state
             };
-            __WEBPACK_IMPORTED_MODULE_0__utils_userRequest__["a" /* default */].getInstance().postData("api/auth/login/wechat", _data).then(function (res) {}).catch();
+            __WEBPACK_IMPORTED_MODULE_0__utils_userRequest__["a" /* default */].getInstance().postData("api/auth/login/wechat", _data).then(function (res) {
+
+                if (!res.data.data.token) {
+                    _this.$router.push("/login/regist/" + "?oauth_user=" + oauth_user);
+                }
+
+                if (res.data.data.oauth_user) {}
+            }).catch();
         }
     }
 
