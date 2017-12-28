18eff18ee5b7da0acfeaff4ab3c246d4f2288de1
luobailiang <luobailiang@gmail.com>
2017-12-28 12:30:52 +0800

    店铺转账

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -9,6 +9,7 @@ use App\ShopFund;
 use App\ShopUser;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
