07a47dfdd339b278a7d853c679f14cfd74610174
lijuan <lijuan@supernano.com>
2017-12-29 17:25:03 +0800

    修复bug

--- resources/views/admin/shopDetail.blade.php
+++ resources/views/admin/shopDetail.blade.php
@@ -2,7 +2,7 @@
         <div class="box box-widget widget-user-2">
             <div class="box-header">
                 <div class="widget-user-header">
-                    <h4 class="widget-user-desc">ID： <span>{{$list->id}}</span></h4>
+                    <h4 class="widget-user-desc">ID： <span>{{\App\Shop::encrypt($list->id)}}</span></h4>
                     <h4 class="widget-user-desc">店主：
                         <div class="user-panel clearfix">
                             <div class="pull-left">
@@ -10,21 +10,21 @@
                             </div>
                             <div class="pull-left ml7">
                                 <p>{{$list->manager['name']}}</p>
-                                <span>ID:<span class="text-yellow">{{$list->manager_id}}</span></span>
+                                <span>ID:<span class="text-yellow">{{\App\User::encrypt($list->manager_id)}}</span></span>
                             </div>
                         </div>
                     </h4>
-                    <h4 class="widget-user-desc">店铺会员（{{$users_arr->count()}}）
-                        @if (!empty($users_arr) && $users_arr->count()>0 )
+                    <h4 class="widget-user-desc">店铺会员（{{$list->users->count()}}）
+                        @if (!empty($list->users->count()) && $list->users->count()>0 )
                         <div class="user-panel clearfix">
-                            @foreach($users_arr as $user_item)
+                            @foreach($list->users as $_user)
                             <div style="margin-left: 20px;float: left">
                                 <div class="pull-left">
-                                    <img src="{{$user_item->avatar}}" width="40" height="40" class="img-circle">
+                                    <img src="{{$_user->avatar}}" width="40" height="40" class="img-circle">
                                 </div>
                                 <div class="pull-left ml7">
-                                    <p>{{$user_item->name}}</p>
-                                    <span>ID:<span class="text-yellow">{{$user_item->id}}</span></span>
+                                    <p>{{$_user->name}}</p>
+                                    <span>ID:<span class="text-yellow">{{\App\User::encrypt($_user->id)}}</span></span>
                                 </div>
                             </div>
                             @endforeach
@@ -34,11 +34,13 @@
                     <h4 class="widget-user-desc">店铺交易笔数：<span>{{$list->transfer_cnt}}</span></h4>
                     <h4 class="widget-user-desc">平台交易费率：<span>{{config('platform_fee_percent') . '%'}}</span></h4>
                     <h4 class="widget-user-desc">已付平台交易费：<span>{{$list->fee_amount_cnt??0}}</span></h4>
-                    <h4 class="widget-user-desc">店铺手续费率：<span>{{(int)$list->fee . '%'}}</span>
+                    <h4 class="widget-user-desc">店铺手续费率：<span>{{$list->fee . '%'}}</span>
                     <h4 class="widget-user-desc">店铺默认单价：<span>{{$list->price}}</span></h4>
                     <h4 class="widget-user-desc">总交易额：<span>{{$list->summary??0}}</span></h4>
                     <h4 class="widget-user-desc">店铺收入：<span>{{$list->tip_amount_cnt??0}}</span></h4>
                     <h4 class="widget-user-desc">店铺余额：<span>{{$list->container['balance']}}</span></h4>
+                    <h4 class="widget-user-desc">店铺状态：<span>{{$list->status>0 ? ($list->status==1?'已解散':'已冻结') :'正常'}}</span></h4>
+                    <h4 class="widget-user-desc">店铺是否开启交易：<span>{{$list->active}}</span></h4>
                     <h4 class="widget-user-desc">店铺创建时间：<span>{{$list->created_at}}</span></h4>
                 </div>
                 <form class="form-horizontal" method="post" action="/admin/shop/updates">
