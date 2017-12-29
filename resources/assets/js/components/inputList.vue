00c213cdbe49125a45360162ba79fba6443b6530
sangliang <378305868@qq.com>
2017-12-29 16:18:44 +0800

    修复一些bug

--- resources/assets/js/components/inputList.vue
+++ resources/assets/js/components/inputList.vue
@@ -74,8 +74,8 @@
     }
 
     .mask{
-        width:100%;
-        height:100%;
+        width:100vw;
+        height:100vh;
         position: absolute;
         background: rgba(0,0,0,0.4);
     }
