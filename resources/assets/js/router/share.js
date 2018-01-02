9eb7cdb1f12841b45632d7cf2d19792a24314f28
liangtao <1174945650@qq.com>
2018-01-02 18:07:34 +0800

    update

--- resources/assets/js/router/share.js
+++ resources/assets/js/router/share.js
@@ -1,6 +1,11 @@
 
 import Share from "../view/Share/share.vue"
-
+import ShareUser from "../view/Share/shareUser.vue"
+import Generalize from "../view/Share/generalize.vue"
+import InviteLink from "../view/Share/inviteLink.vue"
 export default [
-    { path: '/share', name: 'share', component: Share }
+    { path: '/share', name: 'share', component: Share },
+    { path: '/shareUser', name: 'ShareUser', component: ShareUser },
+    { path: '/shareUser/generalize', name: 'Generalize', component: Generalize },
+    { path: '/shareUser/inviteLink', name: 'InviteLink', component: InviteLink }
 ]
