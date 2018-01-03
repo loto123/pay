00c85a05cb629fff0588124337d15772828bcc0e
luobailiang <luobailiang@gmail.com>
2018-01-03 17:53:57 +0800

    update

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -101,7 +101,7 @@ class ShopController extends BaseController {
             return $this->json([], trans("api.error_shop_percent"), 0);
         }
         $user = $this->auth->user();
-        if ($user->shop()->count() >= 30) {
+        if ($user->shop()->count() >= 3) {
             return $this->json([], trans("api.over_max_times"), 0);
         }
         $wallet = PayFactory::MasterContainer();
