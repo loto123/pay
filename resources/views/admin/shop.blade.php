7a80e15098b2dfda8df02034ade9e6a22985d312
lijuan <lijuan@supernano.com>
2018-01-08 19:31:06 +0800

    店铺管理 增加删除，修改详情

--- resources/views/admin/shop.blade.php
+++ resources/views/admin/shop.blade.php
@@ -101,6 +101,9 @@
                                 @endif
                             </td>
                             <td><a href="/admin/shop/detail/{{$item->id}}">详情</a></td>
+                            @if(Admin::user()->isRole('administrator') && $unclose_cnt_list[$item->id] == 0)
+                                <td><a href="/admin/shop/delete/{{$item->id}}">删除</a></td>
+                            @endif
                         </tr>
                     @endforeach
                     </tbody>
