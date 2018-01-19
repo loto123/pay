394a72f84a3b455a1ab7d89f4f19da5114cd7326
sangliang <378305868@qq.com>
2018-01-19 18:32:20 +0800

    修复在我的店铺成员列表中删除自己的bug

--- resources/assets/js/view/Shop/shopMember.vue
+++ resources/assets/js/view/Shop/shopMember.vue
@@ -34,11 +34,11 @@
               </span>
           </li>
 
-          <li class="flex flex-v flex-align-center" v-for="item in dataList">
+          <li class="flex flex-v flex-align-center" v-for="(item,index) in dataList">
               <img :src="item.avatar" alt="" class="avatar">
-              <h3>{{SetString(item.name,6)}}</h3>
-              <span class="notice flex flex-align-center flex-justify-center" v-if="controlSwitch" @click="deleteMember(item.id)">
-                -
+              <h3>{{SetString(item.name,6)}}  </h3>
+              <span class="notice flex flex-align-center flex-justify-center" v-if="controlSwitch &&  index!=0" @click="deleteMember(item.id)">
+                - 
               </span>
           </li>
       </ul>
@@ -213,7 +213,6 @@ export default {
       Loading.getInstance().open();
       this.shopId = this.$route.query.shopId;
       this.isGroupMaster = this.$route.query.isGroupMaster;
-      console.log(this.isGroupMaster);
 
       request.getInstance().getData("api/shop/members/"+this.shopId).then(res=>{
         this.dataList = res.data.data.members;
