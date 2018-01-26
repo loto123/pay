6d6943f4ee06b9954b1694e3c46550b418f043fa
sangliang <378305868@qq.com>
2018-01-26 16:05:21 +0800

    卖狗模块

--- resources/assets/js/view/MyAccount/withdraw.vue
+++ resources/assets/js/view/MyAccount/withdraw.vue
@@ -1,33 +1,42 @@
 <template>
 	<div id="withdraw" class="withdraw-container">
 		<topBack title="出售" style="background: #eee;">
-			<div class="flex flex-reverse" style="width:100%;padding-right:1em;box-sizing:border-box;" @click="goIndex">
-				<i class="iconfont" style="font-size:1.4em;">&#xe602;</i>
+			<div class="flex flex-reverse" style="width:100%;padding-right:1em;box-sizing:border-box;" @click="openOption">
+				<i class="iconfont" style="font-size:1.4em;">&#xe7fe;</i>
 			</div>
 		</topBack>
 		<div class="withdraw-box">
-			<div>
+			<div class="flex flex-v flex-align-start">
 				<div class="tltle">选择要出售的宠物：</div>
 				<div class="pet-list-box">
-					<ul class="pet-list">
-						<li>
-							<img src="">
+					<ul class="pet-list flex flex-justify-around">
+						<li class="flex flex-align-center flex-justify-center active">
+							<img src="/images/avatar.jpg">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg">
 						</li>
 					</ul>
+
 					<div class="look-more">
-						查看更多
+						<mt-button type="primary" size="large" style="height: 100%;" @click="lookMore">查看更多</mt-button>
 					</div>
 				</div>
-				<div>
+
+				<!-- <div>
 					<div>
 						免费领取宠物蛋（剩余：3次）
 					</div>
-				</div>
+				</div> -->
 			</div>
 			<div class="price-list-box">
 				<div class="tltle">出售价格</div>
 				<ul class="price-list flex flex-wrap-on">
-					<li v-for="(item,index) in priceList">￥{{item}}</li>
+					<li v-for="(item,index) in priceList">￥{{item}}
+					</li>
 				</ul>
 				<div class="high-price flex flex-align-center flex-justify-center">¥{{my_max_quota}}(最高价)</div>
 			</div>
@@ -35,7 +44,7 @@
 			<div class="withdraw-way">
 				<div class="title">收款方式</div>
 				<div class="list-wrap">
-					<mt-radio align="right" v-model="value" :options="options1" @change="check">
+					<mt-radio align="right" v-model="value" :options="cardOptions" v-if="isShow">
 					</mt-radio>
 				</div>
 			</div>
@@ -43,6 +52,63 @@
 				<mt-button type="primary" size="large">出售</mt-button>
 			</a>
 		</div>
+
+		<div class="popDetail flex flex-align-center flex-justify-center" v-if="isPopDetailShow">
+			<div class="mask" @touchmove.stop.prevent></div>
+			<div class="content flex flex-v flex-align-center">
+				<div class="title flex flex-align-center flex-justify-center" @touchmove.stop.prevent>
+					<span class="flex-1"></span>
+					<h3 class="flex-8">选择要出售的宠物</h3>
+					<span class="flex-1">
+						<i class="iconfont" style="font-size: 1.2em;" @click="closePanel">&#xe604;</i>
+					</span>
+				</div>
+
+				<div class="pets">
+					<ul class="flex flex-wrap-on">
+						<li class="flex flex-align-center flex-justify-center active">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+						<li class="flex flex-align-center flex-justify-center">
+							<img src="/images/avatar.jpg" alt="">
+						</li>
+					</ul>
+				</div>
+
+				<div class="comfirm-button">
+					<mt-button type="primary" size="large">确定</mt-button>
+				</div>
+
+			</div>
+		</div>
+
 		<passWorld :setSwitch="showPasswordTag" v-on:hidePassword="hidePassword" v-on:callBack="callBack"></passWorld>
 	</div>
 </template>
@@ -58,8 +124,9 @@
 		data() {
 			return {
 				showPasswordTag: false,       // 密码弹出开关
+				dataList:[],
 
-				options1: [],
+				cardOptions: [],
 				way: null,	//提现方式
 				value: null,
 				has_pay_password: null,//是否设置支付密码
@@ -67,6 +134,9 @@
 				my_max_quota:null,
 				fee_value: null,
 				isFee: false,//是否展示手续费
+				isShow:false,
+
+				isPopDetailShow:false,  // 查看更多显示
 
 				priceList:[]	//价格列表
 			}
@@ -79,28 +149,36 @@
 		},
 		components: { topBack, passWorld },
 		methods: {
-			goIndex() {
-				this.$router.push('/index');
+			openOption() {
+				console.log("打开了选项");
+				// this.$router.push('/index');
 			},
 			hidePassword() {
 				this.showPasswordTag = false;
 			},
 			init() {
 				Loading.getInstance().open("加载中...");
-
-				Promise.all([request.getInstance().getData("api/pet/sellable"),request.getInstance().getData("api/account"), request.getInstance().getData('api/account/withdraw-methods')])
+				// /account/withdraw-methods
+				Promise.all([
+					request.getInstance().getData("api/pet/sellable"),
+					request.getInstance().getData("api/account"), 
+					request.getInstance().getData('api/account/withdraw-methods')
+					])
 					.then((res) => {
-						console.log(res[0]);
 						this.balance=res[1].data.data.balance;
 						this.has_pay_password = res[1].data.data.has_pay_password;
+
+						this.dataList = res[2].data.data.methods
 						this.setBankList(res[2]);//获取提现方式列表
 						Loading.getInstance().close();
 					})
 					.catch((err) => {
+						console.error(err);
 						Toast(err.data.msg);
 					})
 			},
 			setBankList(res) {
+				console.log(res);
 				var _tempList = [];
 				for (let i = 0; i < res.data.data.methods.length; i++) {
 					var _t = {};
@@ -108,7 +186,10 @@
 					_t.label = res.data.data.methods[i].label;
 					_tempList.push(_t);
 				}
-				this.options1 = _tempList;
+				this.cardOptions = _tempList;
+				this.value = this.cardOptions[0].value;
+				console.log(this.cardOptions);
+				this.isShow = true;
 			},
 			withdrawBtn() {
 				var self = this;
@@ -145,32 +226,163 @@
 					})
 			},
 			check(){  
-				console.log(this.value);
-			}  
+				for(var i = 0 ; i < this.dataList.length; i++){
+					if(this.value == this.dataList[i].id){
+						this.priceList = this.dataList[i].quota_list;
+					}
+				}
+			},
+
+			closePanel(){
+				this.isPopDetailShow = false;
+			},
+
+			lookMore(){
+				this.isPopDetailShow = true;
+			} 
+		},
+		watch:{
+			value:function(){
+				this.check();
+			}
 		}
 	};
 </script>
 
 <style lang="scss" scoped>
-	@import "../../../sass/oo_flex.scss";
 	.withdraw-container {
 		background: #eee;
 		height: 100vh;
 		padding-top: 2em;
 		box-sizing: border-box;
 	}
+	
+	.popDetail{
+		width: 100%;
+		height: 100vh;
+		position: fixed;
+		top:0;
+		left: 0;
+
+		/*遮罩*/
+		.mask{
+			background: rgba(0,0,0,0.8);
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			top:0;
+			left: 0;
+			z-index: 999;
+		}
+
+		.content{
+			width: 95%;
+			height: 24em;
+			background: #fff;
+			border-radius: 0.4em;
+			z-index: 1000;
+			.title{
+				width: 100%;
+				height: 3em;
+
+				h3{
+					text-align: center;
+					font-size: 1.1em;
+					font-weight: bold;
+				}
+			}
+
+			.pets{
+				width:100%;
+				height: 16em;
+				overflow-y:scroll;
+
+				ul{
+					li{
+						box-sizing: border-box;
+						width: 33%;
+						margin-top:0.2em;
+
+						>img{
+							width:80%;
+							border-radius:0.2em;
+						}
+					}
+
+					.active{
+						>img{
+							border:2px solid #26a2ff;
+						}
+					}
+				}
+			}
+
+			.comfirm-button{
+				margin: 0 auto;
+				margin-top:1em;
+				width: 96%;
+			}
+		}
+
+
+	}
 
 	.withdraw-box {
 		background: #fff;
-		padding: 1em;
-		margin: 0 0.5em;
+		padding-top: 1em;
+		padding-bottom: 1em;
+		padding-left: 0.8em;
+		padding-right: 0.8em;
+		box-sizing: border-box;
 		.tltle {
 			font-size: 1em;
 			color: #999;
 		}
+
+		.pet-list-box{
+			margin:0 auto;
+			margin-top:0.5em;
+			width: 98%;
+			height: 8em;
+			border:1px solid #eee;
+			box-sizing: border-box;
+			border-radius: 0.4em;
+			.pet-list{
+				padding-top:0.5em;
+				padding-bottom: 0.5em;
+				box-sizing: border-box;
+				li{
+					width: 4em;
+					height: 4em;
+					border-radius: 0.2em;
+					box-sizing: border-box;
+					border:1px solid #eee;
+
+					>img{
+						display: block;
+						width: 3.8em;
+						height: 3.8em;
+						border-radius:0.2em;
+					}
+				}
+				
+				/*是否被选中*/
+				.active{
+					border:1px solid #26a2ff;
+				}
+			}
+
+			.look-more{
+				height: 2em;
+				width: 95%;
+				margin: 0 auto;
+			}
+		}
 	}
 
 	.price-list-box {
+		margin-top:0.8em;
+		
 		.tltle {
 			color: #666;
 		}
@@ -185,7 +397,7 @@
 				border-radius: 5px;
 				margin-top: 1em;
 				text-align: center;
-				margin-left:7%;
+				margin-left:5%;
 				&:nth-child(3n+1) {
 					margin-left: 0;
 				}
@@ -211,7 +423,7 @@
 		}	
 	}
 	.withdraw-way {
-		margin-top: 2em;
+		margin-top: 0.8em;
 		.title {
 			color: #999;
 			margin-bottom: 0.5em;
