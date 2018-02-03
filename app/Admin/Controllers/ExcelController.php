ca0077c66091da419475d869d772b2009551de0d
nielixin <303896522@qq.com>
2018-02-03 16:51:00 +0800

    修改bug

--- app/Admin/Controllers/ExcelController.php
+++ app/Admin/Controllers/ExcelController.php
@@ -30,36 +30,36 @@ class ExcelController extends Controller
         }
 
         $table_name = (new Shop)->getTable();
-        $listQuery = Shop::leftJoin('transfer as t',function ($join) use($table_name) {
-            $join->on( $table_name.'.id' ,'=' ,'t.shop_id' )
+        $listQuery = Shop::leftJoin('transfer as t', function ($join) use ($table_name) {
+            $join->on($table_name . '.id', '=', 't.shop_id')
                 ->where('t.status', '=', '3');
         })->leftJoin('transfer_record as tfr', function ($join) {
-            $join->on('tfr.transfer_id', '=', 't.id')->where('tfr.stat', '=' , '2');
-        })->leftJoin('tip_record as tr','tr.transfer_id','=','t.id')
-            ->with(['container','manager'])
-            ->select( DB::raw($table_name.'.*'),
+            $join->on('tfr.transfer_id', '=', 't.id')->where('tfr.stat', '=', '2');
+        })->leftJoin('tip_record as tr', 'tr.transfer_id', '=', 't.id')
+            ->with(['container', 'manager'])
+            ->select(DB::raw($table_name . '.*'),
                 DB::raw('COUNT(t.id) as transfer_cnt'), DB::raw('SUM(tfr.amount) as summary'),
                 DB::raw('SUM(tfr.fee_amount) as fee_amount_cnt '),
                 DB::raw('SUM(tr.amount) as tip_amount_cnt'));
-        if(!empty($manager_id)) {
-            $listQuery->whereHas('manager', function ($query) use($manager_id) {
-                $query->where('mobile',$manager_id);
+        if (!empty($manager_id)) {
+            $listQuery->whereHas('manager', function ($query) use ($manager_id) {
+                $query->where('mobile', $manager_id);
             });
         }
-        if(!empty($shop_id)) {
-            $listQuery->where($table_name.'.id', $shop_id);
+        if (!empty($shop_id)) {
+            $listQuery->where($table_name . '.id', $shop_id);
         }
-        if(!empty($shop_name)) {
-            $listQuery->where($table_name.'.name', 'like', '%'.$shop_name.'%');
+        if (!empty($shop_name)) {
+            $listQuery->where($table_name . '.name', 'like', '%' . $shop_name . '%');
         }
-        if($begin && $end) {
-            $listQuery->where($table_name.'.created_at', '>=', $begin)->where($table_name.'.created_at', '<=', $end);
+        if ($begin && $end) {
+            $listQuery->where($table_name . '.created_at', '>=', $begin)->where($table_name . '.created_at', '<=', $end);
         }
-        $listQuery->groupBy($table_name.'.id')->orderBy('tip_amount_cnt','DESC')->withCount('shop_user');
+        $listQuery->groupBy($table_name . '.id')->orderBy('tip_amount_cnt', 'DESC')->withCount('shop_user');
 
         $list = $listQuery->get();
         $cellData = [
-            ['排名', '店铺ID', '店铺名', '店主ID', '店主名', '店铺会员数', '店铺手续费率', '已付平台交易费', '交易笔数', '总交易额', '店铺收入', '店铺余额','店铺状态','店铺交易状态']
+            ['排名', '店铺ID', '店铺名', '店主ID', '店主名', '店铺会员数', '店铺手续费率', '已付平台交易费', '交易笔数', '总交易额', '店铺收入', '店铺余额', '店铺状态', '店铺交易状态']
         ];
         if (!empty($list) && count($list) > 0) {
             foreach ($list as $key => $value) {
@@ -76,7 +76,7 @@ class ExcelController extends Controller
                     $value->summary ?? 0,
                     $value->tip_amount_cnt ?? 0,
                     $value->container['balance'],
-                    $value->status>0 ? ($value->status==1?'已解散':'已冻结') :'正常',
+                    $value->status > 0 ? ($value->status == 1 ? '已解散' : '已冻结') : '正常',
                     $value->active ? '开启' : '关闭',
                 ];
             }
@@ -91,7 +91,7 @@ class ExcelController extends Controller
     public function user(Request $request)
     {
         $cellData = [
-            ['编号', '用户id','用户名称', '身份', '交易笔数', '余额', '收益', '收款', '付款', '上级运营id','上级运营名称', '上级代理', '支付渠道']
+            ['编号', '用户id', '用户名称', '身份', '交易笔数', '余额', '收益', '收款', '付款', '上级运营id', '上级运营名称', '上级代理', '支付渠道']
         ];
 
         Log::info($request->all());
@@ -179,7 +179,7 @@ class ExcelController extends Controller
             },
             'output_profit' => function ($query) use ($begin, $end) {
                 if ($begin && $end) {
-                    $query->where('transfer_record.created_at', '>=', $begin)->where('transfer_record.created_at', '<=', $end);
+                    $query->where('profit_record.created_at', '>=', $begin)->where('profit_record.created_at', '<=', $end);
                 }
             }
         ])
@@ -243,11 +243,11 @@ class ExcelController extends Controller
                 $item->operator ? $item->operator->id : '无',
                 $item->operator ? $item->operator->name : '无',
                 $item->trans_amount ?? 0,
-                $item->transfer_record()->count(),
-                $item->transfer_record()->where('stat', 2)->sum('amount'),
-                abs($item->transfer_record()->where('stat', 1)->sum('amount')),
+                $item->transfer_record->count(),
+                $item->transfer_record->where('stat', 2)->sum('amount'),
+                abs($item->transfer_record->where('stat', 1)->sum('amount')),
                 $item->balance,
-                $item->output_profit()->sum('fee_amount'),
+                $item->output_profit->sum('fee_amount'),
                 $item->child_user_count,
                 $item->child_proxy_count,
                 $item->proxy_fee_amount ?? 0,
@@ -286,6 +286,8 @@ class ExcelController extends Controller
             $query->where('users.operator_id', $operator);
         }
         $date_time = $request->input('date_time');
+        $begin = '';
+        $end = '';
         if (!empty($date_time)) {
             $date_time_arr = explode(' - ', $request->input('date_time'));
             $begin = $date_time_arr[0];
@@ -304,7 +306,13 @@ class ExcelController extends Controller
             $with[] = 'tips';
             $with[] = 'output_profit';
         }
-        $list = $query->with($with)->leftJoin('profit_record', 'users.id', '=', 'profit_record.user_id')
+        $list = $query->with($with)->leftJoin('profit_record', function ($join) use ($begin, $end) {
+            $join->on('users.id', '=', 'profit_record.user_id');
+            if ($begin && $end) {
+                $join->where('profit_record.created_at', '>=', $begin);
+                $join->where('profit_record.created_at', '<=', $end);
+            }
+        })
             ->select('users.*', DB::raw('SUM(profit_record.fee_amount) as fee_amount_total'))
             ->orderBy('fee_amount_total', 'DESC')->groupBy('users.id')->get();
         foreach ($list as $key => $item) {
@@ -316,11 +324,11 @@ class ExcelController extends Controller
                 $item->proxy ? $item->proxy->name : '无',
                 $item->operator ? $item->operator->id : '无',
                 $item->operator ? $item->operator->name : '无',
-                $item->transfer_record()->count(),
-                $item->transfer_record()->sum('amount'),
-                $item->tips()->sum('amount'),
-                $item->output_profit()->sum('proxy_amount'),
-                $item->output_profit()->sum('fee_amount')
+                $item->transfer_record->count(),
+                $item->transfer_record->sum('amount'),
+                $item->tips->sum('amount'),
+                $item->output_profit->sum('proxy_amount'),
+                $item->output_profit->sum('fee_amount')
             ];
         }
         Excel::create('收益统计', function ($excel) use ($cellData) {
@@ -333,7 +341,7 @@ class ExcelController extends Controller
     //VIP卡-拨卡记录
     public function card_record(Request $request)
     {
-        $cellData = [['序号','制卡人','制卡人ID','拨卡人','拨卡人ID','卡号','收卡人','收卡人ID','拨卡时间']];
+        $cellData = [['序号', '制卡人', '制卡人ID', '拨卡人', '拨卡人ID', '卡号', '收卡人', '收卡人ID', '拨卡时间']];
         $allocate_id = $request->allocate_id;
         $operator_id = $request->operator_id;
         $card_id = $request->card_id;
@@ -350,37 +358,37 @@ class ExcelController extends Controller
 
         $query = CardStock::query()->with(['distributions.promoter', 'allocate_bys', 'operators', 'card']);
         //运营只能看到自己的
-        if(!Admin::user()->can('create_agent_card') && Admin::user()->can('operate_agent_card')) {
-            $query = $query->where('operator',Admin::user()->id);
+        if (!Admin::user()->can('create_agent_card') && Admin::user()->can('operate_agent_card')) {
+            $query = $query->where('operator', Admin::user()->id);
         }
-        if(!empty($allocate_id)) {
-            $query = $query->whereHas('allocate_bys',function($query) use($allocate_id) {
-                $query->where('username',$allocate_id);
+        if (!empty($allocate_id)) {
+            $query = $query->whereHas('allocate_bys', function ($query) use ($allocate_id) {
+                $query->where('username', $allocate_id);
             });
         }
-        if(!empty($operator_id)) {
-            $query = $query->whereHas('operators',function($query) use($operator_id) {
-                $query->where('username',$operator_id);
+        if (!empty($operator_id)) {
+            $query = $query->whereHas('operators', function ($query) use ($operator_id) {
+                $query->where('username', $operator_id);
             });
         }
-        if(!empty($promoter_id)) {
+        if (!empty($promoter_id)) {
             $query = $query->whereHas('distributions', function ($query) use ($promoter_id) {
                 $query->whereHas('promoter', function ($query) use ($promoter_id) {
                     $query->where('mobile', $promoter_id);
                 });
             });
         }
-        if(!empty($card_id)) {
-            $query = $query->where('card_id',$en_card_id);
+        if (!empty($card_id)) {
+            $query = $query->where('card_id', $en_card_id);
         }
-        if(!empty($begin) && !empty($end)) {
-            $query = $query->where('created_at','>=',$begin)->where('created_at','<=',$end);
+        if (!empty($begin) && !empty($end)) {
+            $query = $query->where('created_at', '>=', $begin)->where('created_at', '<=', $end);
         }
         $list = $query->get();
-        if(!empty($list)) {
-            foreach ($list as $key=>$item) {
+        if (!empty($list)) {
+            foreach ($list as $key => $item) {
                 $cellData[] = [
-                    $key+1,
+                    $key + 1,
                     $item->allocate_bys ? $item->allocate_bys['name'] : '无',
                     $item->allocate_bys ? $item->allocate_bys['username'] : '无',
                     $item->operators ? $item->operators['name'] : '无',
@@ -402,7 +410,7 @@ class ExcelController extends Controller
     //VIP卡-查询
     public function cards(Request $request)
     {
-        $cellData = [['序号','卡号','运营','运营ID','推广员','推广员ID','用卡人','用卡人ID','状态','是否冻结']];
+        $cellData = [['序号', '卡号', '运营', '运营ID', '推广员', '推广员ID', '用卡人', '用卡人ID', '状态', '是否冻结']];
         $card_id = $request->card_id;
         $agent_id = $request->agent_id;
         $operator_id = $request->operator_id;
@@ -444,31 +452,31 @@ class ExcelController extends Controller
             $query = $query->where('created_at', '>=', $begin)->where('created_at', '<=', $end);
         }
         $list = $query->get();
-        if(!empty($list)) {
-            foreach ($list as $key=>$item) {
+        if (!empty($list)) {
+            foreach ($list as $key => $item) {
                 $item_owner_name = '无';
                 $item_owner_id = '无';
                 $item_operator_name = '无';
                 $item_operator_id = '无';
-                if($item->is_bound == $item::BOUND  && $owner = $item->owner_user) {
+                if ($item->is_bound == $item::BOUND && $owner = $item->owner_user) {
                     $item_owner_name = $owner['name'];
                     $item_owner_id = $owner['mobile'];
                 }
-                if(!empty($item->stock) && isset($item->stock['operators'])){
+                if (!empty($item->stock) && isset($item->stock['operators'])) {
                     $item_operator_name = $item->stock['operators']['name'];
                     $item_operator_id = $item->stock['operators']['username'];
                 }
                 $cellData[] = [
-                    $key+1,
+                    $key + 1,
                     $item->mix_id(),
                     $item_operator_name,
                     $item_operator_id,
-                    $item->promoter ? $item->promoter['name']  : '无',
-                    $item->promoter ? $item->promoter['mobile']  : '无',
+                    $item->promoter ? $item->promoter['name'] : '无',
+                    $item->promoter ? $item->promoter['mobile'] : '无',
                     $item_owner_name,
                     $item_owner_id,
-                    $item->is_bound?'已使用':'未使用',
-                    $item->is_frozen?'已冻结':'未冻结'
+                    $item->is_bound ? '已使用' : '未使用',
+                    $item->is_frozen ? '已冻结' : '未冻结'
                 ];
             }
         }
