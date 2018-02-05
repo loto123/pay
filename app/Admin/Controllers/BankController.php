98ffaffe37526489d229de753547bb2e9d519cf7
lijuan <lijuan@supernano.com>
2018-02-05 10:42:00 +0800

    修改，注释掉验证银行卡号归属的

--- app/Admin/Controllers/BankController.php
+++ app/Admin/Controllers/BankController.php
@@ -10,6 +10,8 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\MessageBag;
 
 class BankController extends Controller
 {
@@ -59,7 +61,6 @@ class BankController extends Controller
 
             $content->header('header');
             $content->description('创建银行');
-
             $content->body($this->form());
         });
     }
@@ -75,6 +76,8 @@ class BankController extends Controller
             $grid->id('ID')->sortable();
             $grid->name('银行');
             $grid->logo('图标')->image();
+//            $grid->card_num_pre('卡号开头');
+//            $grid->card_num_pre_size('卡号开头长度');
             $grid->created_at('添加时间');
             $grid->updated_at('更新时间');
         });
@@ -89,10 +92,36 @@ class BankController extends Controller
     {
         return Admin::form(Bank::class, function (Form $form) {
             $form->display('id', 'ID');
-            $form->text('name',  '银行');
+            $form->text('name',  '银行')->rules('required');
             $form->image('logo', '图片')->uniqueName();
+//            $form->textarea('card_num_pre','卡号开头')->placeholder('请输入卡号开头，用英文逗号分隔');
+//            $form->text('card_num_pre_size','卡号开头长度')->rules('required|min:0|max:8');
             $form->display('created_at', '添加时间');
             $form->display('updated_at', '更新时间');
+//            $form->saving(function (Form $form) {
+//                if(!empty($form->card_num_pre)) {
+//                    try{
+//                        $bank_card_pre_list = explode(',',$form->card_num_pre);
+//                        foreach ($bank_card_pre_list as $value) {
+//                            if(strlen($value) != $form->card_num_pre_size) {
+//                                $error = new MessageBag([
+//                                    'title'   => '操作失败',
+//                                    'message' => '卡号开头中数据的长度需与卡号开头长度保持一致',
+//                                ]);
+//                                return back()->with(compact('error'));
+//                            }
+//                        }
+//                    } catch (\Exception $e) {
+//                        $error = new MessageBag([
+//                            'title'   => '操作失败',
+//                            'message' => '卡号开头输入有误',
+//                        ]);
+//                        return back()->with(compact('error'));
+//                    }
+//                }
+//
+//            });
+
         });
     }
 }
