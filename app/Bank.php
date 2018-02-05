a94b168f8ff2e15a5a3ebb61451f74e303896bda
lijuan <lijuan@supernano.com>
2018-02-05 16:21:45 +0800

    修改银行卡logo

--- app/Bank.php
+++ app/Bank.php
@@ -9,12 +9,17 @@ class Bank extends Model
 {
     protected $table = 'banks';
 
-    const LOGO_PRE = '/storage/admin/';
+    const LOGO_PRE = '/storage/';
     /**
      * 支持的支付平台
      */
     public function payPlatformSupport()
     {
         return $this->belongsToMany(Platform::class, 'pay_banks_support');
+    }
+
+    public function getLogoAttribute($value)
+    {
+        return $value ? url(self::LOGO_PRE.$value) : asset("images/bank.jpg");
     }
 }
