26d7ad7a78d8340fca5cbb18be4e739e09e67180
lesliewung <vocn2010@gmail.com>
2018-02-06 09:53:46 +0800

    容器交易撤回记录错误

--- app/Pay/Model/Transfer.php
+++ app/Pay/Model/Transfer.php
@@ -7,6 +7,7 @@
 
 namespace App\Pay\Model;
 
+use App\Pay\PayLogger;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
@@ -54,7 +55,8 @@ class Transfer extends Model
     public function chargeback()
     {
         $result = self::CHARGE_BACK_ERR;
-
+        $error_code = 0;
+        $error_amount = null;
         //开始事务
         $commit = false;
         DB::beginTransaction();
@@ -62,6 +64,7 @@ class Transfer extends Model
             //状态检查
             $transfer = Transfer::where('state', self::STATE_COMPLETE)->with(['profitShares.receiveContainer', 'containerFrom', 'containerTo'])->lockForUpdate()->find($this->getKey());
             if (!$transfer) {
+                $error_code = 1;
                 break;
             }
 
@@ -70,6 +73,8 @@ class Transfer extends Model
             foreach ($transfer->profitShares as $profitShare) {
                 if (!$profitShare->receiveContainer->changeBalance($profitShare->is_frozen ? 0 : -$profitShare->amount, $profitShare->is_frozen ? -$profitShare->amount : 0)) {
                     $result = self::CHARGE_BACK_OUT_OF_BALANCE;
+                    $error_code = 2;
+                    $error_amount = $profitShare->amount;
                     break 2;
                 }
                 $profit_share_sum += $profitShare->amount;
@@ -79,17 +84,22 @@ class Transfer extends Model
             $actual_received = $transfer->amount - $transfer->fee - $profit_share_sum;
             if (!$transfer->containerTo->changeBalance($transfer->to_frozen ? 0 : -$actual_received, $transfer->to_frozen ? -$actual_received : 0)) {
                 $result = self::CHARGE_BACK_OUT_OF_BALANCE;
+                $error_code = 3;
+                $error_amount = $actual_received;
                 break;
             }
 
             //资金打回
             if (!$transfer->containerFrom->changeBalance($transfer->from_frozen ? 0 : $transfer->amount, $transfer->from_frozen ? $transfer->amount : 0)) {
+                $error_code = 4;
+                $error_amount = $transfer->amount;
                 break;
             }
 
             //变更状态
             $transfer->state = self::STATE_CHARGEBACK;
             if (!$transfer->save()) {
+                $error_code = 5;
                 break;
             }
 
@@ -100,6 +110,9 @@ class Transfer extends Model
 
         //结束事务
         $commit ? DB::commit() : DB::rollBack();
+        if (!$commit) {
+            PayLogger::common()->error('转账撤回失败', ['code' => $error_code, 'amount' => $error_amount]);
+        }
         return $result;
     }
 
