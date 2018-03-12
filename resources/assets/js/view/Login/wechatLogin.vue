b187f6877a6a8bcdaddb7cc2fa226beb2f1e8ba7
liangtao <1174945650@qq.com>
2018-03-12 17:49:44 +0800

    update

--- resources/assets/js/view/Login/wechatLogin.vue
+++ resources/assets/js/view/Login/wechatLogin.vue
@@ -1,61 +1,60 @@
 <template>
-    <div id="we-chat">
-        微信登录中...
-    </div>
+  <div id="we-chat">
+    微信登录中...
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
 </style>
 
 <script>
 
-import request from '../../utils/userRequest'
-import utils from '../../utils/utils'
-import {Toast} from 'mint-ui'
+  import request from '../../utils/userRequest'
+  import utils from '../../utils/utils'
+  import { Toast } from 'mint-ui'
 
-export default {
-    data(){
-        return {
-            state:null,
-            code:null
-        }
+  export default {
+    data() {
+      return {
+        state: null,
+        code: null
+      }
     },
-  created(){
+    created() {
       this.init();
-  },
-  methods:{
-    init(){
+    },
+    methods: {
+      init() {
         this.code = utils.getQueryString("code");
         this.state = utils.getQueryString("state");
         this.bindMobile = window.location.href.split("mobile=")[1];
-        
+
         var _data = {
-            code :this.code,
-            state:this.state,
+          code: this.code,
+          state: this.state,
         };
-        
-        if(this.bindMobile){
-            _data.user_id = this.bindMobile;
+
+        if (this.bindMobile) {
+          console.log(this.bindMobile);
+          _data.user_ticket = this.bindMobile;
         }
 
-        request.getInstance().postData("api/auth/login/wechat",_data).then(res=>{
+        request.getInstance().postData("api/auth/login/wechat", _data).then(res => {
 
-            if(!res.data.data.token){
-                window.location.href = "/#/login/regist/"+"?oauth_user="+res.data.data.oauth_user;
-            }else {
-                Toast("微信登录成功");
-                request.getInstance().setToken(res.data.data.token);
-                
-                setTimeout(()=>{
-                    window.location.href = "/#/index";
-                },2000);
-            }
-            
+          if (!res.data.data.token) {
+            window.location.href = "/#/login/regist/" + "?oauth_user=" + res.data.data.oauth_user;
+          } else {
+            Toast("微信登录成功");
+            request.getInstance().setToken(res.data.data.token);
+
+            setTimeout(() => {
+              window.location.href = "/#/index";
+            }, 2000);
+          }
+
         }).catch();
+      }
     }
-  }
-  
-}
-</script>
 
+  }
+</script>
