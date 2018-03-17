e5d1d1ecb2dfd6c77a4ea67d3e8510f47d96627d
luobailiang <luobailiang@gmail.com>
2018-03-17 15:36:17 +0800

    update

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -2169,6 +2169,7 @@ class ShopController extends BaseController
      *                      @SWG\Property(property="shop_id", type="string", example="1234567890", description="店铺id"),
      *                      @SWG\Property(property="shop_name", type="string", example="店铺名", description="店铺名"),
      *                      @SWG\Property(property="user_name", type="string", example="1234567890", description="用户名"),
+     *                      @SWG\Property(property="user_avatar", type="string", example="url", description="用户头像"),
      *                  @SWG\Property(property="mode", type="integer", example=1,description="收入支出 0=收入 1=支出"),
      *                  @SWG\Property(property="amount", type="double", example=9.9,description="金额"),
      *                  @SWG\Property(property="created_at", type="integer", example=152000000,description="创建时间戳"),
@@ -2216,6 +2217,7 @@ class ShopController extends BaseController
                 'shop_id' => $_fund->shop ? $_fund->shop->en_id() : "",
                 'shop_name' => $_fund->shop ? $_fund->shop->name : "",
                 'user_name' => $_fund->user ? $_fund->user->name : "",
+                'user_avatar' => $_fund->user ? $_fund->user->avatar : "",
                 'mode' => (int)$_fund->mode,
                 'amount' => (double)$_fund->amount,
                 'created_at' => strtotime($_fund->created_at)
