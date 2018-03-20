b8f7366877e881718d7f09413e8dde97fe6dcf80
lijuan <lijuan@supernano.com>
2018-03-20 14:54:34 +0800

    用户数据鉴权记录

--- app/PayInterfaceRecord.php
+++ app/PayInterfaceRecord.php
@@ -11,4 +11,42 @@ class PayInterfaceRecord extends Model
     const DEAL_SUCCESS = 1;
     const UN_RESPONSE = 2;
     const DEAL_FAIL = 3;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public static function get_type_name($type)
+    {
+        switch ($type) {
+            case '1':
+                return '实名认证';
+            case '2':
+                return '银行卡鉴权';
+            default:
+                return '';
+        }
+    }
+
+    public static function get_status_name($status)
+    {
+        switch ($status) {
+            //0：未提交，1：已提交未响应，2：处理成功，3：处理失败'
+            case '0':
+                return '未提交';
+            case '1':
+                return '匹配成功';
+            case '2':
+                return '已提交';
+            case '3':
+                return '匹配失败';
+            default:
+                return '';
+        }
+    }
+
+
+
+
 }
