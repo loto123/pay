64e9f9d9cda8e9497e686bee1c697b6ef60928dc
sangliang <sangliang@supernano.com>
2018-03-22 14:26:05 +0800

    调试线上问题

--- resources/assets/js/view/MyAccount/bill.vue
+++ resources/assets/js/view/MyAccount/bill.vue
@@ -382,17 +382,24 @@
                                 this.recordList[m].in = res.data.data.in;
                                 this.recordList[m].out = res.data.data.out;
 
-                                if(_tempIndex <this.headList.length){
-                                    ++_tempIndex;
+                                // if(_tempIndex <this.headList.length){
+                                //     ++_tempIndex;
+                                // }
+
+                                for(var j = 0 ; j< this.headList.length; j ++ ){
+                                    if(this.headList[j].time == this.recordList[m].time){
+                                        this.headList[j].in = res.data.data.in;
+                                        this.headList[j].out = res.data.data.out;
+                                    }
                                 }
 
-                                if(_tempIndex > 0){
-                                    this.headList[_tempIndex].in = res.data.data.in;
-                                    this.headList[_tempIndex].out = res.data.data.out;
-                                } else {
-                                    this.headList[this.headList.length - 1].in = res.data.data.in;
-                                    this.headList[this.headList.length - 1].out = res.data.data.out;
-                                }
+                                // if(_tempIndex > 0){
+                                //     this.headList[_tempIndex].in = res.data.data.in;
+                                //     this.headList[_tempIndex].out = res.data.data.out;
+                                // } else {
+                                //     this.headList[this.headList.length - 1].in = res.data.data.in;
+                                //     this.headList[this.headList.length - 1].out = res.data.data.out;
+                                // }
                                 
 
                                 this.timeInfo = this.recordList[0].time;
@@ -411,23 +418,31 @@
                                 this.recordList[m].in = res.data.data.in;
                                 this.recordList[m].out = res.data.data.out;
 
-                                
-                                if(_tempIndex <this.headList.length){
-                                    ++_tempIndex;
-                                }
+                                for(var j = 0 ; j< this.headList.length; j ++ ){
 
-                                console.log("####################");
-                                console.log(_timer);
-                                console.log(_tempIndex);
-                                console.log(this.headList.length);
-                                
-                               if(_tempIndex > 0){
-                                    this.headList[_tempIndex].in = res.data.data.in;
-                                    this.headList[_tempIndex].out = res.data.data.out;
-                                } else {
-                                    this.headList[this.headList.length - 1].in = res.data.data.in;
-                                    this.headList[this.headList.length - 1].out = res.data.data.out;
+                                    if(this.headList[j].time == this.recordList[m].time){
+                                        this.headList[j].in = res.data.data.in;
+                                        this.headList[j].out = res.data.data.out;
+                                    }
+
                                 }
+                                
+                               //  if(_tempIndex <this.headList.length){
+                               //      ++_tempIndex;
+                               //  }
+
+                               //  console.log("####################");
+                               //  console.log(_timer);
+                               //  console.log(_tempIndex);
+                               //  console.log(this.headList.length);
+
+                               // if(_tempIndex > -1 && ){
+                               //      this.headList[_tempIndex].in = res.data.data.in;
+                               //      this.headList[_tempIndex].out = res.data.data.out;
+                               //  } else {
+                               //      this.headList[this.headList.length - 1].in = res.data.data.in;
+                               //      this.headList[this.headList.length - 1].out = res.data.data.out;
+                               //  }
 
                                 this.timeInfo = this.recordList[0].time;
                                 this.tabIncome = this.recordList[0].in; //收入
