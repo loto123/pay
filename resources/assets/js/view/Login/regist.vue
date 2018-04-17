45290739cde58b3c649488b36fa1b6d12dd958c7
liangtao <1174945650@qq.com>
2018-04-17 14:43:19 +0800

    update

--- resources/assets/js/view/Login/regist.vue
+++ resources/assets/js/view/Login/regist.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 注册模块 -->
   <div id="regist">
-    <topBack style="background:#fff;"></topBack>
+    <!-- <topBack style="background:#fff;"></topBack> -->
     <section class="content step1" v-if="$store.state.regist.step==0?true:false">
       <h3>
         请输入推荐码
